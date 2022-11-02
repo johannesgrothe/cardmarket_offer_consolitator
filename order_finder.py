31a0106e815bef909d5b0fbf3af6a2ccf6facfa5
Johannes Grothe <johannesgrothe@web.de>
2022-11-02 22:43:15 +0100

    Performance enhancements

--- order_finder.py
+++ order_finder.py
@@ -1,4 +1,5 @@
 import copy
+import logging
 import sys
 from typing import Optional
 
@@ -8,13 +9,38 @@ from offer_collection import OfferCollection
 
 
 class OrderFinder:
+    _logger: logging.Logger
     all_offers: dict[Card, list[Offer]]
-
     checked_offers: list[int]
 
     def __init__(self, all_offers: dict[Card, list[Offer]]):
-        self.all_offers = all_offers
+        self._logger = logging.getLogger(self.__class__.__name__)
+        self.all_offers = self._remove_single_sellers(all_offers)
         self.checked_offers = []
+
+    def _remove_single_sellers(self, in_data: dict[Card, list[Offer]]) -> dict[Card, list[Offer]]:
+        all_sellers = {}
+        for card, offers in in_data.items():
+            for offer in offers:
+                if offer.seller in all_sellers:
+                    all_sellers[offer.seller] += 1
+                else:
+                    all_sellers[offer.seller] = 1
+
+        double_sellers = [x for x, y in all_sellers.items() if y > 1]
+        out_data = {}
+
+        for card, offers in in_data.items():
+            offers.sort()
+            new_offers = [x for x in offers if x.seller in double_sellers]
+            cheapest_offer = offers[0]
+            offers.sort()
+            if cheapest_offer not in new_offers:
+                new_offers.append(cheapest_offer)
+            self._logger.info(f"Reduced offer-count for '{card.name}' from {len(offers)} to {len(new_offers)}")
+            out_data[card] = new_offers
+
+        return out_data
 
     def find_lowest_offer(self) -> OfferCollection:
         reference_offers = OfferCollection([y[0] for x, y in self.all_offers.items()])
